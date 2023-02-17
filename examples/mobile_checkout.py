86ed8e0a9b742243edaa49e38318344f55cc72ed
Kalebu <isaackeinstein@gmail.com>
2023-02-17 13:34:49 +0300

    updated base_url

--- examples/mobile_checkout.py
+++ examples/mobile_checkout.py
@@ -11,11 +11,14 @@ gateway = Azampay(
     client_id=os.getenv("CLIENT_ID"),
     client_secret=os.getenv("CLIENT_SECRET"),
     x_api_key=os.getenv("X_API_KEY"),
+    base_url="https://checkout.azampay.co.tz",
+    auth_url="https://authenticator.azampay.co.tz",
+    sandbox=False,
 )
 
 
 response = gateway.mobile_checkout(
-    mobile="0667863412", amount=1000, provider="tigo", external_id="123456789"
+    mobile="255657649154", amount=1000, external_id="123456789"
 )
 
 print(response)
